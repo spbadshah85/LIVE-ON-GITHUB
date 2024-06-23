cf3c936a8f74f34415de9571bfde6fed7dcd6184
spbadshah85 <spbadshah85@gmail.com>
2024-06-23 20:13:37 +0530

    six

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout<<"hggjhg";
+    cout<<"sunil kumar";
+    
+
+
+
+{  
+    cout<<"hggjhg";
+    cout<<"sunil kumar singh kushwaha";
+    return 0;
+}}
