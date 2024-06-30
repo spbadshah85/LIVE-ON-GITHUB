b81f6ba688e9c8cc21e916612601fba2ca0d665d
spbadshah85 <spbadshah85@gmail.com>
2024-06-30 20:35:52 +0530

    first

---
+++ .vscode/fg.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("sunil");
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}
--- cout.cpp
+++ cout.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 int main(int argc, char const *argv[])
-{
+{  
     cout<<"hggjhg";
-    cout<<"sunil kumar singh";
+    cout<<"sunil kumarsingh";
     return 0;
 }
